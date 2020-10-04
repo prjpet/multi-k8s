2e5d13c1acffe1f204a4a575e6b478973090f643
Peter <peter.prjevara@gmail.com>
2020-10-04 10:29:12 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+docker build -t peterprjevara/multi-client:latest -t peterprjevara/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t peterprjevara/multi-server:latest -t peterprjevara/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t peterprjevara/multi-worker:latest -t peterprjevara/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push peterprjevara/multi-client:latest
+docker push peterprjevara/multi-client:$SHA
+
+docker push peterprjevara/multi-server:latest
+docker push peterprjevara/multi-server:$SHA
+
+docker push peterprjevara/multi-worker:latest
+docker push peterprjevara/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=peterprjevara/multi-server:$SHA
+kubectl set image deployment/client-deployment server=peterprjevara/multi-client:$SHA
+kubectl set image deployment/worker-deployment server=peterprjevara/multi-worker:$SHA
+
+
